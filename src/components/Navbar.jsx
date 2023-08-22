0de0ba9693b168cd9a66cc5ab20e9b81dbbdc5ed
wlsgul326@gmail.com <wlsgul326@gmail.com>
2023-08-23 00:59:58 +0900

    [Navbar] make navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { FiSearch } from 'react-icons/fi';
 
 export default function Navbar() {
-  return <nav>Navbar</nav>;
+  return (
+    <div className='text-xl flex justify-between border-b-2'>
+      <nav className='my-1'>
+        <Link to='/products'>Products</Link>
+      </nav>
+      <form action=''>
+        <input type='text' placeholder='search..' className='outline-none' />
+        <button>
+          <FiSearch />
+        </button>
+      </form>
+    </div>
+  );
 }
