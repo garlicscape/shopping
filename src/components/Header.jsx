8adc8e5eb29d89cfa486e89d4725f9ecbd51c64d
wlsgul326@gmail.com <wlsgul326@gmail.com>
2023-09-14 23:55:57 +0900

    [Header, Navbar] style
    헤더 글씨 변경, 네비게이션 메뉴 스타일링

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,11 +11,11 @@ export default function Header() {
   const { user, login, logout } = useAuthContext();
 
   return (
-    <header className='my-2 flex '>
+    <header className='my-6 flex '>
       <Link to='/' className='flex items-center m-auto'>
         <IoIosBasket className='text-sky-950 text-5xl mr-1' />
-        <h3 className='text-sky-900 text-4xl font-bold'>SHOP</h3>
-        <h3 className='text-sky-600 text-4xl font-bold'>US</h3>
+        <h3 className='text-sky-900 text-5xl font-bold'>SHOP</h3>
+        <h3 className='text-sky-600 text-5xl font-bold'>US</h3>
       </Link>
       <nav className='flex items-center gap-3 font-bold'>
         {user && <UserAvatar user={user} />}
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,9 +4,9 @@ import { FiSearch } from 'react-icons/fi';
 
 export default function Navbar() {
   return (
-    <div className='text-xl flex justify-between border-b-2'>
-      <nav className='my-1'>
-        <Link to='/products'>Products</Link>
+    <div className='flex justify-between items-center border-b-2'>
+      <nav className='my-1 text-lg hover:[&>*]:text-gray-600 [&>*]:mr-4 '>
+        <Link to='/products'>전체상품</Link>
         <Link to='/products/outer'>아우터</Link>
         <Link to='/products/top'>상의</Link>
         <Link to='/products/pants'>팬츠</Link>
@@ -15,8 +15,12 @@ export default function Navbar() {
         <Link to='/products/shoes'>신발</Link>
       </nav>
       <form action=''>
-        <input type='text' placeholder='search..' className='outline-none' />
-        <button>
+        <input
+          type='text'
+          placeholder='search..'
+          className='outline-none hidden md:inline'
+        />
+        <button className='text-xl hover:scale-105 transition-all ease-out duration-300'>
           <FiSearch />
         </button>
       </form>
