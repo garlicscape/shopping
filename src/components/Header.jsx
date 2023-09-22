44d2924683c70151abbcf0efc953eca92836ca0b
wlsgul326@gmail.com <wlsgul326@gmail.com>
2023-09-22 11:44:32 +0900

    [Header] add login menu in user toggle menu
    모바일 화면에서 나오는 사용자 토글 메뉴에 로그인도 포함

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,9 +2,9 @@ import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { IoIosBasket } from 'react-icons/io';
 import { BsCart } from 'react-icons/bs';
+import Button from '../components/ui/Button';
 import UserAvatar from './UserAvatar';
 import { BiPencil } from 'react-icons/bi';
-import Button from '../components/ui/Button';
 import { useAuthContext } from './context/AuthContext';
 import { RiArrowDownSLine, RiArrowUpSLine } from 'react-icons/ri';
 
@@ -48,16 +48,16 @@ export default function Header() {
           className={`
           ${slideToggle ? 'block' : 'hidden'}               
           ${toggle ? 'animate-slide-down' : 'animate-slide-up'}
-          max-[639px]:absolute max-[639px]:top-11  max-[639px]:right-24 max-[639px]:z-10 
-            max-[639px]:rounded-b-md  max-[639px]:bg-white max-[639px]:shadow-lg
-            text-lg 
-            sm:text-2xl sm:flex sm:gap-2 
-            md:gap-3`}
+          max-[639px]:absolute max-[639px]:top-11 max-[639px]:right-0 max-[639px]:z-10 
+          max-[639px]:rounded-b-md max-[639px]:w-28 max-[639px]:h-[7.25rem] max-[639px]:bg-white max-[639px]:shadow-xl max-[639px]:flex-col
+          text-lg flex
+          sm:text-2xl sm:gap-2 sm:items-center sm:flex
+          md:gap-3`}
         >
           {user && (
             <Link
               to='/carts'
-              className='max-[639px]:pt-4 max-[639px]:px-3 group max-[639px]:flex max-[639px]:relative'
+              className='max-[639px]:pt-4 group max-[639px]:flex max-[639px]:justify-center max-[639px]:relative'
             >
               <BsCart className='z-10' />
               <p className='ml-1 z-10 text-sm inline sm:hidden'>장바구니</p>
@@ -67,30 +67,17 @@ export default function Header() {
           {user && user.isAdmin && (
             <Link
               to='/products/new'
-              className='max-[639px]:py-3 max-[639px]:pt-4 max-[639px]:px-3 group max-[639px]:flex max-[639px]:relative'
+              className='max-[639px]:py-2 max-[639px]:pt-4 group max-[639px]:flex max-[639px]:justify-center max-[639px]:relative'
             >
               <BiPencil className='z-10' />
               <p className='ml-1 z-10 text-sm inline sm:hidden'>상품등록</p>
-              <span className='inline absolute h-10 top-2 w-0 left-0 bg-slate-100 rounded-b-md transition-all duration-300 group-hover:w-full sm:hidden'></span>
+              <span className='inline absolute h-9 top-2 w-0 left-0 bg-slate-100 transition-all duration-300 group-hover:w-full sm:hidden'></span>
             </Link>
           )}
+          {!user && <Button text='Login' onClick={login} />}
+          {user && <Button text='Logout' onClick={logout} />}
         </div>
-        {!user && <Button text='Login' onClick={login} />}
-        {user && <Button text='Logout' onClick={logout} />}
       </nav>
     </header>
   );
 }
-/*
-(toggle ? (
-            <RiArrowUpSLine
-              className='text-xl cursor-pointer sm:hidden'
-              onClick={() => setToggle(!toggle)}
-            />
-          ) : (
-            <RiArrowDownSLine
-              className='text-xl cursor-pointer sm:hidden'
-              onClick={() => setToggle(!toggle)}
-            />
-          ))
-*/
