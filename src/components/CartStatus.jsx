ddee62f15cc1fc5c77e454c2f4ae5505de197ac4
wlsgul326@gmail.com <wlsgul326@gmail.com>
2023-09-23 14:32:26 +0900

    [Header] add cart status
    장바구니에 제품이 있으면 장바구니 아이콘에 개수가 나오도록 함

---
+++ src/components/CartStatus.jsx
@@ -0,0 +1,18 @@
+import { useQuery } from '@tanstack/react-query';
+import React from 'react';
+import { getCart } from '../api/firebase';
+import { useAuthContext } from './context/AuthContext';
+
+export default function CartStatus() {
+  const { user } = useAuthContext();
+  const { data: products } = useQuery(['carts'], () => getCart(user.uid));
+  return (
+    <div className='relative'>
+      {products && (
+        <p className='hidden sm:inline absolute -top-8 -right-2 w-5 h-5 leading-5 rounded-full bg-sky-600 text-white text-center text-sm'>
+          {products.length}
+        </p>
+      )}
+    </div>
+  );
+}
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,6 +7,7 @@ import UserAvatar from './UserAvatar';
 import { BiPencil } from 'react-icons/bi';
 import { useAuthContext } from './context/AuthContext';
 import { RiArrowDownSLine, RiArrowUpSLine } from 'react-icons/ri';
+import CartStatus from './CartStatus';
 
 export default function Header() {
   const { user, login, logout } = useAuthContext();
@@ -62,6 +63,7 @@ export default function Header() {
               <BsCart className='z-10' />
               <p className='ml-1 z-10 text-sm inline sm:hidden'>장바구니</p>
               <span className='inline absolute h-10 top-1 w-0 left-0 bg-slate-100 transition-all duration-300  group-hover:w-full sm:hidden'></span>
+              <CartStatus />
             </Link>
           )}
           {user && user.isAdmin && (
