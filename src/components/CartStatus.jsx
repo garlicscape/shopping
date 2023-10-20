ddee8389070a6e59fa11dbcb6041e1dc09dd1947
wlsgul326@gmail.com <wlsgul326@gmail.com>
2023-10-20 23:24:28 +0900

    [Cart] correct cart item total quantity method
    장바구니 아이콘에 장바구니에 들어있는 아이템 개수를 띄우는 방법을 수정

--- src/components/CartStatus.jsx
+++ src/components/CartStatus.jsx
@@ -6,11 +6,16 @@ export default function CartStatus() {
     cartQuery: { data: products },
   } = useCart();
 
+  const sumQuantity = Object.values(products).reduce(
+    (acc, product) => acc + product.quantity,
+    0
+  );
+
   return (
     <>
       {products && (
         <p className='hidden sm:inline absolute -top-2 -right-2 w-5 h-5 leading-5 rounded-full bg-sky-600 text-white text-center text-sm'>
-          {products.length}
+          {sumQuantity}
         </p>
       )}
     </>
