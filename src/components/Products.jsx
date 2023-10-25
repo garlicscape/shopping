5d68d6b30d62e4441f537edc2b0a0a67ece4d1e0
wlsgul326@gmail.com <wlsgul326@gmail.com>
2023-10-25 23:48:02 +0900

    [Products] move a little down on screen

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -3,7 +3,7 @@ import ProductCard from './ProductCard';
 
 export default function Products({ products }) {
   return (
-    <ul className='grid grid-cols-2 gap-10 my-6 md:grid-cols-3 xl:grid-cols-4'>
+    <ul className='grid grid-cols-2 gap-10 mb-6 mt-12 md:grid-cols-3 xl:grid-cols-4'>
       {products.map((product) => (
         <ProductCard key={product.id} product={product} />
       ))}
