d436684c936a8a430f581877c3b8fe513c09a660
wlsgul326@gmail.com <wlsgul326@gmail.com>
2023-10-26 15:26:48 +0900

    [Cart] change cartstatus code
    장바구니에 아무것도 없으면 숫자가 안 뜨도록 조건을 바꿈

--- src/components/CartStatus.jsx
+++ src/components/CartStatus.jsx
@@ -15,7 +15,7 @@ export default function CartStatus() {
 
   return (
     <>
-      {products && (
+      {sumQuantity !== 0 && (
         <p className='hidden sm:inline absolute -top-2 -right-2 w-5 h-5 leading-5 rounded-full bg-sky-600 text-white text-center text-sm'>
           {sumQuantity}
         </p>
